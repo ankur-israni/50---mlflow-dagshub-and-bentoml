6cc5577ffa27c931c603f8972fab8f430f602056
Ankur Israni <onlyankur@gmail.com>
2026-04-02 20:03:48 -0500

    local mlflow working

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
-# The data set used in this example is from http://archive.ics.uci.edu/ml/datasets/Wine+Quality
-# P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.
-# Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
-
 import os
 import warnings
 import sys
+import logging
+from pathlib import Path
 
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
-from urllib.parse import urlparse
 import mlflow
-from mlflow.models import infer_signature
 import mlflow.sklearn
-
-import logging
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
@@ -29,70 +23,69 @@ def eval_metrics(actual, pred):
     return rmse, mae, r2
 
 
+# Main program starts here
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(40)
 
-    # Read the wine-quality csv file from the URL
-    csv_url = (
-        "https://raw.githubusercontent.com/mlflow/mlflow/master/tests/datasets/winequality-red.csv"
-    )
+    # Load trackling uri - Local or Remote here
+    BASE_DIR = Path(__file__).resolve().parent
+    db_uri = f"sqlite:///{BASE_DIR / 'mlflow.db'}"
+    artifact_uri = (BASE_DIR / "mlruns").resolve().as_uri()
+    mlflow.set_tracking_uri(db_uri)
+    print("tracking uri:", mlflow.get_tracking_uri())
+    print("artifact uri:", artifact_uri)
+
+    # Set experiment metadata
+    experiment_name = "wine-quality-experiment"
+    exp = mlflow.get_experiment_by_name(experiment_name)
+    if exp is None:
+        mlflow.create_experiment(
+            name=experiment_name,
+            artifact_location=artifact_uri
+        )
+    mlflow.set_experiment(experiment_name)
+
+
+    # Load dataset
+    csv_url = "https://raw.githubusercontent.com/mlflow/mlflow/master/tests/datasets/winequality-red.csv" # Dataset
     try:
         data = pd.read_csv(csv_url, sep=";")
     except Exception as e:
         logger.exception(
             "Unable to download training & test CSV, check your internet connection. Error: %s", e
         )
+        raise
 
-    # Split the data into training and test sets. (0.75, 0.25) split.
-    train, test = train_test_split(data)
-
-    # The predicted column is "quality" which is a scalar from [3, 9]
+    # Train, Test
+    train, test = train_test_split(data, random_state=42)
     train_x = train.drop(["quality"], axis=1)
     test_x = test.drop(["quality"], axis=1)
-    train_y = train[["quality"]]
-    test_y = test[["quality"]]
+    train_y = train["quality"]
+    test_y = test["quality"]
 
+    # Hyper-parameters for Elasticnet ML algorithm
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+
 
     with mlflow.start_run():
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
 
         predicted_qualities = lr.predict(test_x)
+        rmse, mae, r2 = eval_metrics(test_y, predicted_qualities)
 
-        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+        print(f"Elasticnet model (alpha={alpha:.6f}, l1_ratio={l1_ratio:.6f}):")
+        print(f"  RMSE: {rmse}")
+        print(f"  MAE: {mae}")
+        print(f"  R2: {r2}")
 
-        print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
-        print("  RMSE: %s" % rmse)
-        print("  MAE: %s" % mae)
-        print("  R2: %s" % r2)
-
+        # Log these params for MLFlow recording
         mlflow.log_param("alpha", alpha)
         mlflow.log_param("l1_ratio", l1_ratio)
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
-        #predictions = lr.predict(train_x)
-        #signature = infer_signature(train_x, predictions)
-
-        ## For Remote server only(DAGShub)
-
-        remote_server_uri="https://dagshub.com/krishnaik06/mlflowexperiments.mlflow"
-        mlflow.set_tracking_uri(remote_server_uri)
-
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-
-        # Model registry does not work with file store
-        if tracking_url_type_store != "file":
-            # Register the model
-            # There are other ways to use the Model Registry, which depends on the use case,
-            # please refer to the doc for more information:
-            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            mlflow.sklearn.log_model(
-                lr, "model", registered_model_name="ElasticnetWineModel"
-            )
-        else:
-            mlflow.sklearn.log_model(lr, "model")
+        mlflow.sklearn.log_model(sk_model=lr, name="model")
--- readme/README.md
+++ readme/README.md
@@ -1,7 +1,60 @@
-## ML FLow experiements
+# 1) Local Run 
+------------------------------------------------
+# Start the Virtual Environment
+Terminal (/Users/ankur/backup/delta/sn/datascience/workspace) >
+conda activate venv/
 
+# Install dependencies
+Terminal ('/Users/ankur/backup/delta/sn/datascience/workspace/udemy - complete datascience ml_dl_nlp bootcamp/50 - mlflow dagshub and bentoml/resources') > 
+pip install -r requirements.txt
+
+# Run the python script
+Terminal ('/Users/ankur/backup/delta/sn/datascience/workspace/udemy - complete datascience ml_dl_nlp bootcamp/50 - mlflow dagshub and bentoml') > 
+python app.py
+
+# Start the MLFlow UI (using 'mlflow.db' file as backend-store)
+Terminal ('/Users/ankur/backup/delta/sn/datascience/workspace/udemy - complete datascience ml_dl_nlp bootcamp/50 - mlflow dagshub and bentoml') > 
+mlflow server --backend-store-uri sqlite:///mlflow.db --port 6001
+OR
+mlflow ui --backend-store-uri sqlite:///mlflow.db --port 6001
+
+
+# Start the MLFlow UI (using 'mlruns' folder as the backend-store) - This is deprecated as of Feb 2026
+Terminal ('/Users/ankur/backup/delta/sn/datascience/workspace/udemy - complete datascience ml_dl_nlp bootcamp/50 - mlflow dagshub and bentoml') > 
+mlflow server --backend-store-uri ./mlruns --port 6001
+
+
+# View MLFlow on Browser
+Browser > http://localhost:6001
+
+
+# Stop the Virtual Environment
+------------------------------------------------
+Terminal (/Users/ankur/backup/delta/sn/datascience/workspace) >
+conda deactivate
+
+# 2) Remote Run (Dagshub) - TODO
+------------------------------------------------
+
+## ML FLow experiements
 MLFLOW_TRACKING_URI=https://dagshub.com/krishnaik06/mlflowexperiments.mlflow \
 MLFLOW_TRACKING_USERNAME=krishnaik06 \
 MLFLOW_TRACKING_PASSWORD=7104284f1bb44ece21e0e2adb4e36a250ae3251f \
 python script.py
+
+
+# 3) Git Repository
+------------------------------------------------
+This folder '50 - mflow dagshub and bemtoml' has its own .git repository
+Remote URL = git@github.com:ankur-israni/50---mlflow-dagshub-and-bentoml.git
+
+# 4) Querying SQLLite > mlflow.db 
+------------------------------------------------
+Terminal ('/Users/ankur/backup/delta/sn/datascience/workspace/udemy - complete datascience ml_dl_nlp bootcamp/50 - mlflow dagshub and bentoml') > 
+ - sqlite3 mlflow.db
+ - select * from runs
+ - select * from experiments;
+ - SELECT * FROM metrics WHERE key = 'rmse';
+ - SELECT r.run_uuid, m.value FROM runs r JOIN metrics m ON r.run_uuid = m.run_uuid WHERE m.key = 'rmse' ORDER BY m.value ASC;
+
 
